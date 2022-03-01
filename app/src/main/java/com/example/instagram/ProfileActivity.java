4291c2cdda0d032f562ad4762944c5f975295050
rhp34 <rhp34@njit.edu>
2022-03-01 11:06:35 -0500

    updated

---
+++ app/src/main/java/com/example/instagram/ProfileActivity.java
@@ -0,0 +1,23 @@
+package com.example.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private Button btnSignout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        btnSignout = findViewById(R.id.btnSignout);
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/instagram/TimelineActivity.java
+++ app/src/main/java/com/example/instagram/TimelineActivity.java
@@ -1,11 +1,17 @@
 package com.example.instagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ActionMenuView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -19,6 +25,7 @@ public class TimelineActivity extends AppCompatActivity {
     RecyclerView rvPosts;
     List<Post> posts;
     TimelineAdapter adapter;
+    Toolbar tbToolbar;
 
     public static final String TAG = "TimelineActivity";
     private final int REQUEST_CODE = 905;
@@ -28,16 +35,40 @@ public class TimelineActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
+
         rvPosts = findViewById(R.id.rvPosts);
         posts = new ArrayList<>();
         adapter = new TimelineAdapter(this, posts);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         rvPosts.setLayoutManager(layoutManager);
         rvPosts.setAdapter(adapter);
+        getSupportActionBar().setIcon(R.drawable.nux_dayone_landing_logo);
 
         populateTimeline();
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.mCamera){
+            goMainActivity();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
 
     private void populateTimeline() {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
